4500aa001df2277dda5ae3ef107e141ac3532fd6
Ali Og Oi <114064816+AliOgOi@users.noreply.github.com>
2024-11-20 22:25:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const menuList = document.querySelector('.menu ul');
 const sections = document.querySelectorAll('.section');
+const music = document.getElementById('background-music');
+const toggleButton = document.getElementById('toggle-music');
+
+toggleButton.addEventListener('click', () => {
+    if (music.paused) {
+        music.play();
+        toggleButton.textContent = 'Tắt nhạc';
+    } else {
+        music.pause();
+        toggleButton.textContent = 'Bật nhạc';
+    }
+});
 
 menuToggle.addEventListener('click', () => {
     menuList.classList.toggle('active');
